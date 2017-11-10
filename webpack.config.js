3f5f2aa38a8e9524d6328bcb6f526abe1493e732
felixerdy <felix.erdmann@uni-muenster.de>
2017-11-10 16:29:32 +0100

    design

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         })
 
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   },
   output: {
