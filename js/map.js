ec64b06172513007097ece7e0d8dafcc0c891757
Nico Steffens <n.steffens@uni-muenster.de>
2017-11-10 20:20:38 +0100

    Reworked a few things.. Map now working.

--- js/map.js
+++ js/map.js
@@ -1,5 +1,19 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiZmVsaXhhZXRlbSIsImEiOiI2MmE4YmQ4YjIzOTI2YjY3ZWFmNzUwOTU5NzliOTAxOCJ9.nshlehFGmK_6YmZarM2SHA';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
+let accessToken = 'pk.eyJ1IjoiZmVsaXhhZXRlbSIsImEiOiI2MmE4YmQ4YjIzOTI2YjY3ZWFmNzUwOTU5NzliOTAxOCJ9.nshlehFGmK_6YmZarM2SHA';
+
+let heritage_ms;
+
+$.getJSON('/data/heritage_ms.json', (data) => {
+    heritage_ms = data;
+    
+    const map = new L.map('map').setView([51.964711, 7.628496], 12);
+
+    const basemap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: accessToken
+    }).addTo(map);
+
+    let heritageLayer = L.geoJSON(heritage_ms).addTo(map);
 });
+
